284cb082986a080202b6f49bc74f5875105baa91
Paulo Ricardo Horta Grando <paulorhg@hotmail.com>
2020-08-27 14:47:39 -0300

    AStar

--- A(Star) For Pathfinding/Assets/FollowPath.cs
+++ A(Star) For Pathfinding/Assets/FollowPath.cs
@@ -21,12 +21,44 @@ public class FollowPath : MonoBehaviour
     {
         wps = wpManager.GetComponent<WPManager>().waypoints;
         g = wpManager.GetComponent<WPManager>().graph;
-        currentNode = wps[7];
+        currentNode = wps[0];
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        currentNode = g.getPathPoint(currentWP);
+
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+            currentWP++;
+
+        if(currentWP < g.getPathLength())
+        {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction),
+                                                                                                 rotSpeed * Time.deltaTime);
+
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
     }
+
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[3]);
+        currentWP = 0;
+    }
+
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[0]);
+        currentWP = 0;
+    }
+
 }
